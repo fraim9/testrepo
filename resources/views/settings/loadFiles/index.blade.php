2408f729748f3c296eb6ad2ee4b2f7228a0274bd
Silaev Roman <silaev.r.a@gmail.com>
2019-08-19 18:38:14 +0300

    fixed load files form

--- resources/views/settings/loadFiles/index.blade.php
+++ resources/views/settings/loadFiles/index.blade.php
@@ -9,8 +9,8 @@
     
     <div class="content content-full">
     	<div class="container">
-    	    <div class="row justify-content-center">
-    	        <div class="col-md-8">
+    	    <div class="row">
+    	        <div class="col-xl-4 col-lg-6">
     	        
     	        	<div class="block">
     					<form method="post" action="{{ route('loadFiles.store') }}" enctype="multipart/form-data">
@@ -18,48 +18,37 @@
         	        		<div class="block-header block-header-default">
         	        			<h3 class="block-title">{{ __('Person mass destruction') }}</h3>
         	        		</div>
-        					<div class="block-content">
-    							
-    							<div class="row">
-    								<div class="col-md-4">
-    									{{ __('Select a file') }}
-    								</div>
-    								<div class="col-md-6">
-    									<input type="hidden" name="typeOfFile" value="PersonMassDestruction">
-    									<input type="file" name="xmlfile" id="xmlfile">
-    								</div>
-    							</div>
-    							
-    							
-    							<br><br>		
+        					<div class="block-content py-3">
+								<input type="hidden" name="typeOfFile" value="PersonMassDestruction">
+								<input type="file" name="xmlfile" id="xmlfile" required>
         					</div>
         					
         					<div class="block-footer block-footer-default">
-                            	<div class="row">
-                                    <div class="col-md-3 offset-md-4">
-                                        <button type="submit" class="btn btn-primary btn-lg ladda-button" 
-                                        	data-style="expand-right">{{ __('Load') }}</button>
-                                    </div>
-                                    <div class="col-md-5 text-right">
-                                    	@if (isset($data['PersonMassDestructionLoaded']))
-                                    		@php
-                                    		$action = $data['PersonMassDestructionLoaded'];
-                                    		@endphp
-                                    		<div>
-                                            	<span class="text-muted text-small">{{ __('Last update') }}: </span>
-                                            	@include('helpers.viewDate', ['value' => $action->date, 'format' => 'd M Y H:i'])
-                                        	</div>
-                                        	<div>
-                                            	<span class="text-muted text-small">{{ __('User') }}: </span>
-                                            	{{ $action->user->display_name }}
-                                            </div>
-                                        @endif
-                                    </div>
+                                <div class="mb-3">
+                                    <button type="submit" class="btn btn-primary btn-lg ladda-button" 
+                                    	data-style="expand-right">{{ __('Load') }}</button>
+                                </div>
+                                <div class="">
+                                	@if (isset($data['PersonMassDestructionLoaded']))
+                                		@php
+                                		$action = $data['PersonMassDestructionLoaded'];
+                                		@endphp
+                                		<div>
+                                        	<span class="text-muted text-small">{{ __('Last update') }}: </span>
+                                        	@include('helpers.viewDate', ['value' => $action->date, 'format' => 'd M Y H:i'])
+                                    	</div>
+                                    	<div>
+                                        	<span class="text-muted text-small">{{ __('User') }}: </span>
+                                        	{{ $action->user->display_name }}
+                                        </div>
+                                    @endif
                                 </div>
                             </div>
         					
     					</form>
     				</div>
+   				</div>
+   				<div class="col-xl-4 col-lg-6">
     	        
     	        	<div class="block">
     					<form method="post" action="{{ route('loadFiles.store') }}" enctype="multipart/form-data">
@@ -67,93 +56,67 @@
         	        		<div class="block-header block-header-default">
         	        			<h3 class="block-title">{{ __('Person blocked') }}</h3>
         	        		</div>
-        					<div class="block-content">
+        					<div class="block-content py-3">
     							
-    							<div class="row">
-    								<div class="col-md-4">
-    									{{ __('Select a file') }}
-    								</div>
-    								<div class="col-md-6">
-    									<input type="hidden" name="typeOfFile" value="PersonBlocked">
-    									<input type="file" name="xmlfile" id="xmlfile">
-    								</div>
-    							</div>
+								<input type="hidden" name="typeOfFile" value="PersonBlocked">
+								<input type="file" name="xmlfile" id="xmlfile" required>
     							
-    							
-    							<br><br>		
         					</div>
         					
         					<div class="block-footer block-footer-default">
-                            	<div class="row">
-                                    <div class="col-md-3 offset-md-4">
-                                        <button type="submit" class="btn btn-primary btn-lg ladda-button" 
-                                        	data-style="expand-right">{{ __('Load') }}</button>
-                                    </div>
-                                    <div class="col-md-5 text-right">
-                                    	@if (isset($data['PersonBlokedLoaded']))
-                                    		@php
-                                    		$action = $data['PersonBlokedLoaded'];
-                                    		@endphp
-                                        	<div>
-                                            	<span class="text-muted text-small">{{ __('Last update') }}: </span>
-                                            	@include('helpers.viewDate', ['value' => $action->date, 'format' => 'd M Y H:i'])
-                                        	</div>
-                                        	<div>
-                                            	<span class="text-muted text-small">{{ __('User') }}: </span>
-                                            	{{ $action->user->display_name }}
-                                            </div>
-                                        @endif
-                                    </div>
+                                <div class="mb-3">
+                                    <button type="submit" class="btn btn-primary btn-lg ladda-button" 
+                                    	data-style="expand-right">{{ __('Load') }}</button>
                                 </div>
+                            	@if (isset($data['PersonBlokedLoaded']))
+                            		@php
+                            		$action = $data['PersonBlokedLoaded'];
+                            		@endphp
+                                	<div>
+                                    	<span class="text-muted text-small">{{ __('Last update') }}: </span>
+                                    	@include('helpers.viewDate', ['value' => $action->date, 'format' => 'd M Y H:i'])
+                                	</div>
+                                	<div>
+                                    	<span class="text-muted text-small">{{ __('User') }}: </span>
+                                    	{{ $action->user->display_name }}
+                                    </div>
+                                @endif
                             </div>
         					
     					</form>
     				</div>
-    	        
+   				</div>
+   				<div class="col-xl-4 col-lg-6">
+   				
     	        	<div class="block">
     					<form method="post" action="{{ route('loadFiles.store') }}" enctype="multipart/form-data">
     						@csrf
         	        		<div class="block-header block-header-default">
         	        			<h3 class="block-title">{{ __('Terrorist') }}</h3>
         	        		</div>
-        					<div class="block-content">
-    							
-    							<div class="row">
-    								<div class="col-md-4">
-    									{{ __('Select a file') }}
-    								</div>
-    								<div class="col-md-6">
-    									<input type="hidden" name="typeOfFile" value="Terrorist">
-    									<input type="file" name="xmlfile" id="xmlfile">
-    								</div>
-    							</div>
-    							
-    							
-    							<br><br>		
+        					<div class="block-content py-3">
+								<input type="hidden" name="typeOfFile" value="Terrorist">
+								<input type="file" name="xmlfile" id="xmlfile" required>
         					</div>
         					
         					<div class="block-footer block-footer-default">
-                            	<div class="row">
-                                    <div class="col-md-3 offset-md-4">
+                                    <div class="mb-3">
                                         <button type="submit" class="btn btn-primary btn-lg ladda-button" 
                                         	data-style="expand-right">{{ __('Load') }}</button>
                                     </div>
-                                    <div class="col-md-5 text-right">
-                                    	@if (isset($data['TerroristLoaded']))
-                                    		@php
-                                    		$action = $data['TerroristLoaded'];
-                                    		@endphp
-                                        	<div>
-                                            	<span class="text-muted text-small">{{ __('Last update') }}: </span>
-                                            	@include('helpers.viewDate', ['value' => $action->date, 'format' => 'd M Y H:i'])
-                                        	</div>
-                                        	<div>
-                                            	<span class="text-muted text-small">{{ __('User') }}: </span>
-                                            	{{ $action->user->display_name }}
-                                            </div>
-                                        @endif
-                                    </div>
-                                </div>
+                                	@if (isset($data['TerroristLoaded']))
+                                		@php
+                                		$action = $data['TerroristLoaded'];
+                                		@endphp
+                                    	<div>
+                                        	<span class="text-muted text-small">{{ __('Last update') }}: </span>
+                                        	@include('helpers.viewDate', ['value' => $action->date, 'format' => 'd M Y H:i'])
+                                    	</div>
+                                    	<div>
+                                        	<span class="text-muted text-small">{{ __('User') }}: </span>
+                                        	{{ $action->user->display_name }}
+                                        </div>
+                                    @endif
                             </div>
         					
     					</form>
